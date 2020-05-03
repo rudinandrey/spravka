bb7a9150c407fe243f6ef5afcdb587c2ac0b3d0a
rudinandrey <rudinandrey@yandex.ru>
2020-05-03 03:33:11 +0300

    Начало проекта, добавление f3, подключение к базе данных

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
 
+require("vendor/autoload.php");
+
+$connect = parse_ini_file("/etc/connect.ini");
+
+
+$f3 = \Base::instance();
+$db = new \DB\SQL("mysql:host=db;dbname={$connect["MYSQL_DATABASE"]};charset=utf8", $connect["MYSQL_USER"], $connect["MYSQL_PASSWORD"]);
+
+
+$test = new \Tataisneft\Spravka\Test();
+echo $test->index();
