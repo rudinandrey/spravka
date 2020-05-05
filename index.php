2f14591e6525f8b684b340520bce5e7bbf6056e1
rudinandrey <rudinandrey@yandex.ru>
2020-05-05 04:24:14 +0300

    Вернули index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require("vendor/autoload.php");
+
+$connect = parse_ini_file("/etc/connect.ini");
+
+
+$f3 = \Base::instance();
+$db = new \DB\SQL("mysql:host=db;dbname={$connect["MYSQL_DATABASE"]};charset=utf8", $connect["MYSQL_USER"], $connect["MYSQL_PASSWORD"]);
+$f3->set("DB", $db);
+
+$f3->set("AUTOLOAD", "app/");
+$f3->set("ROOT", ".");
+$f3->set("UI", "./ui/");
+
+$f3->route("GET /", "main->index");
+$f3->route("GET /login", "main->login");
+$f3->route("GET|POST /api/@method", "api->@method");
+
+
+
+$f3->run();
