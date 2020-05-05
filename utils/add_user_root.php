54171ae0e41bf2ff78e9aad0f792b3669f945e8b
rudinandrey <rudinandrey@yandex.ru>
2020-05-06 02:50:34 +0300

    Исправление программы добавления пользователя

--- utils/add_user_root.php
+++ utils/add_user_root.php
@@ -2,7 +2,7 @@
 
 require("../config.php");
 
-$userAdapter = new \Tataisneft\Adapter\UserAdapter();
+$userAdapter = new \Tataisneft\Adapter\UserAdapter($db);
 $user = $userAdapter->create(["user_id"=>0, "email"=>"rudinandrey@yandex.ru", "password"=>"123", "username"=>"Андрей"]);
 print_r($user->get());
 
