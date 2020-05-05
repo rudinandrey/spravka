71db364b3852379ab772a58da3664954873b1a8b
rudinandrey <rudinandrey@yandex.ru>
2020-05-05 22:38:57 +0300

    Добавление тестирования

---
+++ tests/SpravkaTest.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+class SpravkaTest extends \PHPUnit\Framework\TestCase {
+    public function testTrue() {
+        $this->assertTrue(true);
+    }
+}
