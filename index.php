eddcb8422eeb8ed6d96af9a94047194e1a5fa87d
Andrey Rudin <rudinandrey@yandex.ru>
2020-05-08 16:58:03 +0300

    Исправлен маршрут для тестирования

--- index.php
+++ index.php
@@ -8,6 +8,6 @@ $f3->route("GET /login", "enter->login");
 $f3->route("POST /auth", "enter->auth");
 $f3->route("GET|POST /api/@method", "api->@method");
 $f3->route("GET /@method", "main->@method");
-$f3->route("GET /test/@area/@method", "Test/@area->@method");
+$f3->route("GET /test/@area/@method", 'Test\@area->@method');
 
 $f3->run();
