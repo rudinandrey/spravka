4b912788b077ec01c1c0df30d2bc1eb67ab6fdd6
Andrey Rudin <rudinandrey@yandex.ru>
2020-05-08 16:54:44 +0300

    добавили возможность тестировать api на сервере

---
+++ classes/Test/user.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test;
+
+class user extends Controller {
+    public function __construct() {
+    
+    }
+    
+    public function ping($f3) {
+        $this->getResult(["request"=>"pong"]);
+    }
+}
