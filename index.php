3532296932861776d23a251d6bb94e56ced2b797
rudinandrey <rudinandrey@yandex.ru>
2020-05-08 16:29:53 +0300

    добавлен метод авторизации /auth в маршрутах и на странице login

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("config.php");
 
 $f3->route("GET /", "main->index");
 $f3->route("GET /login", "enter->login");
+$f3->route("POST /auth", "enter->auth");
 $f3->route("GET|POST /api/@method", "api->@method");
 $f3->route("GET /@method", "main->@method");
 
--- ui/login.php
+++ ui/login.php
@@ -31,20 +31,20 @@
     <link href="/css/signin.css" rel="stylesheet">
   </head>
   <body class="text-center">
-    <form class="form-signin">
-  <img class="mb-4" src="/images/TatAISneft_Logo.png" alt="">
-  <h1 class="h3 mb-3 font-weight-normal">Авторизуйтесь</h1>
-  <label for="inputEmail" class="sr-only">Email-адрес</label>
-  <input type="email" id="inputEmail" class="form-control" placeholder="Email адрес" required autofocus>
-  <label for="inputPassword" class="sr-only">Пароль</label>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required>
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me"> Запомнить меня
-    </label>
-  </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Вход</button>
-  <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
-</form>
+    <form class="form-signin" method="POST" action="/auth">
+      <img class="mb-4" src="/images/TatAISneft_Logo.png" alt="">
+      <h1 class="h3 mb-3 font-weight-normal">Авторизуйтесь</h1>
+      <label for="inputEmail" class="sr-only">Email-адрес</label>
+      <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email адрес" required autofocus>
+      <label for="inputPassword" class="sr-only">Пароль</label>
+      <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Пароль" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Запомнить меня
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Вход</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
+    </form>
 </body>
 </html>
