1c5aaebcc45896e2e6524cf069dd30c3744cc252
rudinandrey <rudinandrey@yandex.ru>
2020-05-11 23:37:36 +0300

    пробный парсер для es09

--- src/Spravka/Es09Mapper/SearchMapper.php
+++ src/Spravka/Es09Mapper/SearchMapper.php
@@ -18,7 +18,8 @@ class SearchMapper implements SearchInterface {
         ];
         $params = http_build_query($arr);
         $url = "http://es09.ru:8080/api/company/search?city=1604000014&limit=40&offset=0&q=%D0%BA%D0%BE%D1%84%D0%B5";
-        return file_get_contents($url);
+        $data = file_get_contents($url);
+        return $this->parse($data);
     }
 
     public function searchSimple(int $city, string $search) {
@@ -30,8 +31,46 @@ class SearchMapper implements SearchInterface {
         ];
         $params = http_build_query($arr);
         $url = "http://es09.ru:8080/api/company/search?city=1604000014&limit=40&offset=0&q=%D0%BA%D0%BE%D1%84%D0%B5";
-        return file_get_contents($url);
+        $data = file_get_contents($url);
+        return $this->parse($data);
     }
 
+    private function parse($data) {
+        $json = json_decode($data, true);
+
+        $companies = [];
+
+        $results = $json["results"];
+        foreach($results as $result) {
+            if($result["type"] == "company") {
+                $company = [
+                    "name" => $result["name"],
+                    "address" => $result["addressText"],
+                    "info" => $result["info"]
+                ];
+                $phones = $result["phones"];
+                foreach($phones as $phone) {
+                    $phoneAndOwner = $this->parsePhone($phone);
+                    $company["owner"] = $phoneAndOwner["owner"];
+                    $company["phone"] = $phoneAndOwner["phone"];
+                    $companies[] = $company;
+                }
+            }
+        }
+        return $companies;
+    }
+
+    private function parsePhone($phone) {
+        $result = [
+            "phone"=>"",
+            "owner"=>""
+        ];
+        $parts = explode(" ", $phone);
+        foreach($parts as $part) {
+            if(preg_match("/^\d+$/", trim($part)) == true) $result["phone"] = $part;
+            if(preg_match("/\((\D+.)\)/", trim($part), $m)) echo $result["owner"] = $m[1];
+        }
+        return $result;
+    }
 
 }
