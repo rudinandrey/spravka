a23bfeab403200d7014a218190b41017b2e10f25
rudinandrey <rudinandrey@yandex.ru>
2020-05-11 03:29:40 +0300

    отображение только разрешенных абонентов

--- src/Spravka/SqlMapper/SearchMapper.php
+++ src/Spravka/SqlMapper/SearchMapper.php
@@ -17,12 +17,12 @@ class SearchMapper implements SearchInterface {
     }
 
     public function searchSimple($city, $search) {
-        $sql = "SELECT * FROM phonebook WHERE city = :city AND (name LIKE :search OR address LIKE :search);";
+        $sql = "SELECT * FROM phonebook WHERE city = :city AND (name LIKE :search OR address LIKE :search) AND is_visible = 1;";
         return $this->db->exec($sql, ["city"=>$city, "search"=>$search."%"]);
     }
 
     public function search($city, $type, $searchName, $searchAddress) {
-        $sql = "SELECT * FROM phonebook WHERE city = :city AND is_company = :type AND name LIKE :name AND address LIKE :address";
+        $sql = "SELECT * FROM phonebook WHERE city = :city AND is_company = :type AND name LIKE :name AND address LIKE :address  AND is_visible = 1;";
         return $this->db->exec($sql, ["city"=>$city, "type"=>$type, "name"=>$searchName."%", "address"=>$searchAddress."%"]);
     }
 }
