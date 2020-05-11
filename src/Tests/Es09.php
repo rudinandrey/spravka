f268008792da484903d2045ca6a8b2c94d3bd4ea
rudinandrey <rudinandrey@yandex.ru>
2020-05-11 23:02:03 +0300

    тест для проверки es09 для поиска

---
+++ src/Tests/Es09.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+class Es09 {
+
+    public function test1() {
+        echo "es09 test";
+    }
+
+    public function test2() {
+        $url = "http://es09.ru:8080/api/company/search?city=1604000014&limit=40&offset=0&q=%D0%BA%D0%BE%D1%84%D0%B5";
+        $result = file_get_contents($url);
+        echo "<pre>";
+        print_r($result);
+        echo "</pre>";
+    }
+}
